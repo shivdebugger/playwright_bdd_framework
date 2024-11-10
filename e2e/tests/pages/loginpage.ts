f24bf97143f4a76289f28e7f71299803885f1293
shivdebugger <shivrajhippargikc@gmail.com>
2024-11-10 11:12:09 +0530

    Removed locators folder & added the locators in page object file.And also added readme file

--- e2e/tests/pages/loginpage.ts
+++ e2e/tests/pages/loginpage.ts
@@ -1,9 +1,18 @@
 import {Page} from "playwright";
-import * as loginpageloc from "../locators/loginpageloc.json"
 import BasePage from "./basepage";
 import { expect } from "playwright/test";
+
 export default class LoginPage  extends BasePage{
-   
+
+    private userNameField = "//input[@id='auth_user_username']";
+    private userPasswordfield = "//input[@id='auth_user_password']";
+    private loginBtn = "//button[@id='login_button']";
+    private dashoardTxt = "//h2[normalize-space(text())='overview']";
+    private rememberMeChkBox = "//input[@id='auth_user_remember_me']";
+    private lostPasswordbtn = "//a[contains(text(),'Lost Password')]";
+    private forgotPasswordbtn = "//a[contains(text(),'Forgot Username')]";
+    private invalidLoginError = "//div[normalize-space(text())='Invalid login']";
+
     constructor(page:Page){
         super(page)
     }
@@ -13,34 +22,34 @@ export default class LoginPage  extends BasePage{
     };
 
     async enterLoginDetails(username: string, password: string){
-        await this.clearText(loginpageloc.userNameField.locator)
-        await this.enterText(loginpageloc.userNameField.locator,username);
-        await this.clearText(loginpageloc.userPasswordfield.locator);
-        await this.enterText(loginpageloc.userPasswordfield.locator,password);
+        await this.clearText(this.userNameField)
+        await this.enterText(this.userNameField,username);
+        await this.clearText(this.userPasswordfield);
+        await this.enterText(this.userPasswordfield,password);
     };
 
     async clickLoginBtn(){
-        await this.clickElement(loginpageloc.loginBtn.locator);
+        await this.clickElement(this.loginBtn);
     };
 
     async isUserLoggedIn(){
-       await this.verifyElementVisible(loginpageloc.dashoardTxt.locator);
+       await this.verifyElementVisible(this.dashoardTxt);
     };
 
     async isErrorMsgDisplayed(){
-        await this.verifyElementVisible(loginpageloc.invalidLoginError.locator);
+        await this.verifyElementVisible(this.invalidLoginError);
     }    
 
     async verifyLoginResult(expectedOverviewText: string) {
     
-        const isOverviewVisible = await this.page.waitForSelector(loginpageloc.dashoardTxt.locator,{ timeout: 5000 }).catch(() => false);
-        const isErrorVisible = await this.page.isVisible(loginpageloc.invalidLoginError.locator,{timeout: 5000 }).catch(() => false);
+        const isOverviewVisible = await this.page.waitForSelector(this.dashoardTxt,{ timeout: 5000 }).catch(() => false);
+        const isErrorVisible = await this.page.isVisible(this.invalidLoginError,{timeout: 5000 }).catch(() => false);
     
         if (isOverviewVisible) {
-          const actualText = await this.page.locator(loginpageloc.dashoardTxt.locator).innerText();
+          const actualText = await this.page.locator(this.dashoardTxt).innerText();
           expect(actualText.trim()).toBe(expectedOverviewText);
         } else if (isErrorVisible) {
-          const errorText = await this.page.locator(loginpageloc.invalidLoginError.locator).innerText();
+          const errorText = await this.page.locator(this.invalidLoginError).innerText();
           expect(errorText.trim()).toBe(expectedOverviewText);
         } else {
           throw new Error('Neither overview nor error message is visible.');
